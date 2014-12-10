0a9baaea9ea545c3c960d41cf94964c7cf7ecb26
RITVIKMANDALA <RITVIKMANDALA@KSU.EDU>
2014-12-09 19:40:07 -0600

    PROJECT 6

---
+++ Ksu.Cis300.GraphDraw/UserInterface.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Ksu.Cis300.GraphDraw
+{
+    partial class UserInterface
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uxGraphDrawing = new Ksu.Cis300.GraphDrawing.GraphDrawing();
+            this.uxLoad = new System.Windows.Forms.Button();
+            this.uxOpenDialog = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // uxGraphDrawing
+            // 
+            this.uxGraphDrawing.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.uxGraphDrawing.AutoScroll = true;
+            this.uxGraphDrawing.GraphSize = new System.Drawing.Size(150, 150);
+            this.uxGraphDrawing.Location = new System.Drawing.Point(0, 0);
+            this.uxGraphDrawing.Name = "uxGraphDrawing";
+            this.uxGraphDrawing.Size = new System.Drawing.Size(353, 231);
+            this.uxGraphDrawing.TabIndex = 0;
+            // 
+            // uxLoad
+            // 
+            this.uxLoad.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.uxLoad.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxLoad.Location = new System.Drawing.Point(121, 237);
+            this.uxLoad.Name = "uxLoad";
+            this.uxLoad.Size = new System.Drawing.Size(107, 33);
+            this.uxLoad.TabIndex = 1;
+            this.uxLoad.Text = "Load a File";
+            this.uxLoad.UseVisualStyleBackColor = true;
+            this.uxLoad.Click += new System.EventHandler(this.uxLoad_Click);
+            // 
+            // uxOpenDialog
+            // 
+            this.uxOpenDialog.FileName = "openFileDialog1";
+            // 
+            // UserInterface
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(353, 282);
+            this.Controls.Add(this.uxLoad);
+            this.Controls.Add(this.uxGraphDrawing);
+            this.MinimumSize = new System.Drawing.Size(369, 320);
+            this.Name = "UserInterface";
+            this.Text = "Minimum Cost Spanning Trees";
+         
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private GraphDrawing.GraphDrawing uxGraphDrawing;
+        private System.Windows.Forms.Button uxLoad;
+        private System.Windows.Forms.OpenFileDialog uxOpenDialog;
+    }
+}
+
---
+++ Ksu.Cis300.GraphDraw/UserInterface.cs
@@ -0,0 +1,129 @@
+﻿/* UserInterface.cs
+ * Author: Ritvik Mandala
+ * Home Work Assignment 6
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Ksu.Cis300.GraphDrawing;
+using Ksu.Cis300.Graphs;
+using System.IO;
+
+namespace Ksu.Cis300.GraphDraw
+{
+    /// <summary>
+    /// Class of the program that finds a minimum spanning tree.
+    /// </summary>
+    public partial class UserInterface : Form
+    {
+        /// <summary>
+        /// Construct the GUI
+        /// </summary>
+        public UserInterface()
+        {
+            InitializeComponent();
+        }
+        
+        /// <summary>
+        /// Handles uxLoad event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void uxLoad_Click(object sender, EventArgs e)
+        {
+          DirectedGraph<Point, double> graphs = new DirectedGraph<Point, double>();
+          List<Point> _nodes = new List<Point>();
+         if (uxOpenDialog.ShowDialog() == DialogResult.OK)
+            {
+                uxGraphDrawing.Clear();
+                using (StreamReader sr = new StreamReader(uxOpenDialog.FileName))
+                {
+                    string[] graphSize = sr.ReadLine().Split(',');
+                    Size size = new Size(Convert.ToInt32(graphSize[0]), Convert.ToInt32(graphSize[1]));
+                    uxGraphDrawing.GraphSize = size;
+                    while (!sr.EndOfStream)
+                    {
+                        string[] points = sr.ReadLine().Split(',');
+                        int x = Convert.ToInt32(points[0]);
+                        int y = Convert.ToInt32(points[1]);
+                        Point point = new Point(x, y);
+                        _nodes.Add(point);
+                        graphs.AddNode(point);
+                        uxGraphDrawing.DrawPoint(point);
+                    }
+                }
+                foreach (Point a in _nodes)
+                {
+                    foreach (Point b in _nodes)
+                    {
+                        if (b != a)
+                        {
+                            graphs.AddEdge(a, b, EdgeCalc(a, b));
+                        }
+                    }
+                }
+            }
+            try
+            {
+                Point start = _nodes[0];
+                double totalCost = 0;
+                Dictionary<Point, Tuple<Point, double>> dict = new Dictionary<Point, Tuple<Point, double>>();
+                foreach (Tuple<Point, double> tup in graphs.OutgoingEdges(start))
+                {
+                    Tuple<Point, double> t = new Tuple<Point, double>(start, tup.Item2);
+                    dict.Add(tup.Item1, t);
+                }
+                while (dict.Count > 0)
+                {
+                    double cost = Double.PositiveInfinity;
+                    Point point = new Point(0, 0);
+                    foreach (KeyValuePair<Point, Tuple<Point, double>> kvp in dict)
+                    {
+                        if (cost > kvp.Value.Item2)
+                        {
+                            cost = kvp.Value.Item2;
+                            point = kvp.Key;
+                            start = kvp.Value.Item1;
+                        }
+                    }
+                    uxGraphDrawing.DrawLine(point, start);
+                    totalCost += cost;
+                    dict.Remove(point);
+                    foreach (Tuple<Point, double> tup in graphs.OutgoingEdges(point))
+                    {
+                        if (dict.ContainsKey(tup.Item1))
+                        {
+                            if (tup.Item2 < dict[tup.Item1].Item2)
+                            {
+                                Tuple<Point, double> temp = new Tuple<Point, double>(point, tup.Item2);
+                                dict[tup.Item1] = temp;
+                            }
+                        }
+                    }
+                }
+                MessageBox.Show("The total cost is " + totalCost);
+            }
+            catch (Exception ex)
+            {
+                ex.ToString();
+                MessageBox.Show("Total cost: 0");
+            }
+        }
+        private double EdgeCalc(Point x, Point y)
+        {
+            double xSum = y.X - x.X;
+            xSum *= xSum;
+            double ySum = y.Y- x.Y;
+            ySum *= ySum;
+            double edge = Math.Sqrt(xSum + ySum);
+            return edge;
+
+        }
+    }
+}
